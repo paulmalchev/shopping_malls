fafa04b79c31314ebb84ade4e7ba5728badccc26
paulmalchev <pavlomalchev001@gmail.com>
2024-07-13 13:55:14 +0300

    Delete SQL/Screenshots/categories.sql

--- SQL/Screenshots/categories.sql
+++
@@ -1,8 +0,0 @@
-SELECT 
-	category,
-	ROUND(AVG(price_usd), 2) AS avg_price, 
-	SUM(quantity) AS sold_quantity,
-	SUM(price_usd * quantity) AS total_revenue
-FROM shopping_data
-GROUP BY category
-ORDER BY total_revenue DESC
