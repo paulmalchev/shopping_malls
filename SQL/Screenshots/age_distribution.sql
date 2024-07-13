267eb7d0f75021cfabb7ee7c0c0f6f5e3da74f7a
paulmalchev <pavlomalchev001@gmail.com>
2024-07-13 13:54:39 +0300

    Delete SQL/Screenshots/age_distribution.sql

--- SQL/Screenshots/age_distribution.sql
+++
@@ -1,7 +0,0 @@
-SELECT 
-	age, 
-	COUNT(DISTINCT customer_id) AS count, 
-	ROUND(AVG(price_usd * quantity), 2) AS avg_check,
-	SUM(price_usd * quantity) AS total_amount
-FROM shopping_data
-GROUP BY age
